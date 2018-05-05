93d89132ba684075aef83874b16fd05f82e8ba87
Kubing Lin <t6m8@hotmail.com>
2018-05-06 00:27:46 +0800

    Lecture: statements and expressions

---
+++ Section 2/2-JS-Basics/script.js
@@ -0,0 +1,217 @@
+// console.log("Hello World!");
+
+/////////////////////////////////////////////
+// Lecture: variables
+/*
+var name = "John";
+console.log(name);
+
+var lastName = "Smith";
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+*/
+
+/////////////////////////////////////////////
+// Lecture: variables 2
+/*
+var name = "John";
+var age = 26;
+
+// console.log(name + age);
+// console.log(age + name);
+
+var job, isMarried;
+
+// console.log(job);
+
+job = "teacher";
+isMarried = false;
+
+console.log(
+  name +
+    " is a " +
+    age +
+    " years old " +
+    job +
+    ". Is he married? " +
+    isMarried +
+    "."
+);
+
+age = "thirty six";
+job = "driver";
+
+console.log(
+  name +
+    " is a " +
+    age +
+    " years old " +
+    job +
+    ". Is he married? " +
+    isMarried +
+    "."
+);
+
+// var lastName = prompt("What is th last name?");
+// console.log(lastName);
+
+alert(
+  name +
+    " is a " +
+    age +
+    " years old " +
+    job +
+    ". Is he married? " +
+    isMarried +
+    "."
+);
+*/
+
+/////////////////////////////////////////////
+// Lecture: operators
+/*
+var now = 2016;
+var birthYear = now - 26;
+
+birthYear = now - 26 * 2;
+//2016 - 52
+//1964
+
+console.log(birthYear);
+
+var ageJohn = 30;
+var ageMark = 30;
+
+ageJohn = ageMark = (3 + 5) * 4 - 6;
+//ageJohn = ageMark = 26
+//ageJohn = 26;
+ageJohn++;
+ageMark *= 2;
+ageMark = ageMark * 2;
+
+console.log(ageJohn);
+console.log(ageMark);
+*/
+
+/////////////////////////////////////////////
+// Lecture: if/else statements
+/*
+var name = "John";
+var age = 26;
+var isMarried = "no";
+
+if (isMarried === "yes") {
+  console.log(name + " is Married!");
+} else {
+  console.log(name + " will hopefully marry soon :)");
+}
+
+isMarried = true;
+
+// if (isMarried) {
+//   console.log("YES!");
+// } else {
+//   console.log("NO!");
+// }
+
+if (isMarried) {
+  console.log("YES!");
+}
+
+if (23 === "23") {
+  console.log("Something to print...");
+}
+*/
+
+/////////////////////////////////////////////
+// Lecture: boolean logic and switch
+/*
+var age = 20;
+
+if (age < 20) {
+  console.log("John is a teenager");
+} else if (age >= 20 && age < 30) {
+  console.log("John is a young man.");
+} else {
+  console.log("John is a man.");
+}
+
+var job = "teacher";
+
+job = prompt("What does John do?");
+
+switch (job) {
+  case "teacher":
+    console.log("John teaches kids.");
+    break;
+  case "driver":
+    console.log("John drives a cab in Lisbon.");
+    break;
+  case "cop":
+    console.log("John helps fight crime.");
+    break;
+  default:
+    console.log("John does something else.");
+    break;
+}
+*/
+
+/////////////////////////////////////////////
+// Code Challenge 1
+
+/////////////////////////////////////////////
+// Lecture: Functions
+/*
+function calculateAge(yearOfBirth) {
+  var age = 2016 - yearOfBirth;
+  return age;
+}
+
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1969);
+var ageMary = calculateAge(1948);
+console.log(ageMary);
+
+function yearsUntilRetirement(name, year) {
+  var age = calculateAge(year);
+  var retirement = 65 - age;
+
+  if (retirement >= 0) {
+    console.log(name + " retires in " + retirement + " years.");
+  } else {
+    console.log(name + " is already retired.");
+  }
+}
+
+yearsUntilRetirement("John", 1990);
+yearsUntilRetirement("Mike", 1969);
+yearsUntilRetirement("Mary", 1948);
+*/
+
+/////////////////////////////////////////////
+// Lecture: statements and expressions
+
+function someFun(par) {
+  //code
+}
+
+var someFun = function(par) {
+  //code
+};
+
+//Expressions
+3 + 4;
+var x = 3;
+
+//Statements
+if (x === 5) {
+  // do something
+}
+
+/////////////////////////////////////////////
+// Lecture: Arrays
