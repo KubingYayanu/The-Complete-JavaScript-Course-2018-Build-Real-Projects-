a7c6a7c818f873d4c924745955e8c9147a75ecc7
Kubing Lin <t6m8@hotmail.com>
2018-05-06 16:50:13 +0800

    Lecture: The this keyword

--- Section 3/3-How-JS-Works/script.js
+++ Section 3/3-How-JS-Works/script.js
@@ -1,25 +1,35 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
+/*
+// functions
+calculateAge(1965);
 
+function calculateAge(year) {
+  console.log(2016 - year);
+}
 
+// retirement(1956);
 
+var retirement = function(year) {
+  console.log(65 - (2016 - year));
+};
 
+// variables
 
+console.log(age);
+var age = 23;
 
-
-
-
-
-
-
-
-
-
-
+function foo() {
+  console.log(age);
+  var age = 65;
+  console.log(age);
+}
+foo();
+console.log(age);
+*/
 
 ///////////////////////////////////////
 // Lecture: Scoping
-
 
 // First scoping example
 
@@ -37,8 +47,6 @@ function first() {
     }
 }
 */
-
-
 
 // Example to show the differece between execution stack and scope chain
 
@@ -62,16 +70,38 @@ function third() {
 }
 */
 
-
-
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+// console.log(this);
 
+// calculateAge(1985);
 
+// function calculateAge(year) {
+//   console.log(2016 - year);
+//   console.log(this);
+// }
 
+var john = {
+  name: "John",
+  yearOfBirth: 1990,
+  calculateAge: function() {
+    console.log(this);
+    console.log(2016 - this.yearOfBirth);
 
+    // function innerFunction() {
+    //   console.log(this);
+    // }
+    // innerFunction();
+  }
+};
 
+john.calculateAge();
 
+var mike = {
+  name: "Mike",
+  yearOfBirth: 1984
+};
 
-
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
