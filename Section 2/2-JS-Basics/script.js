3dc95ce941a8209e7b1481eb7ec01de241ed79e3
Kubing Lin <t6m8@hotmail.com>
2018-05-06 12:54:34 +0800

    Lecture: Loops

--- Section 2/2-JS-Basics/script.js
+++ Section 2/2-JS-Basics/script.js
@@ -195,7 +195,7 @@ yearsUntilRetirement("Mary", 1948);
 
 /////////////////////////////////////////////
 // Lecture: statements and expressions
-
+/*
 function someFun(par) {
   //code
 }
@@ -212,6 +212,158 @@ var x = 3;
 if (x === 5) {
   // do something
 }
+*/
 
 /////////////////////////////////////////////
 // Lecture: Arrays
+/*
+var names = ["John", "Jane", "Mark"];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[2]);
+names[1] = "Ben";
+console.log(names);
+
+var john = ["John", "Smith", 1990, "designer", false];
+
+john.push("blue");
+john.unshift("Mr.");
+john.pop();
+john.shift();
+console.log(john);
+
+if (john.indexOf("teacher") === -1) {
+  console.log("John is NOT a teacher.");
+}
+*/
+
+/////////////////////////////////////////////
+// Lecture: Objects
+/*
+var john = {
+  name: "John",
+  lastName: "Smith",
+  yearOfBirth: 1990,
+  job: "teacher",
+  isMarried: false
+};
+
+console.log(john.lastName);
+console.log(john["lastName"]);
+
+var xyz = "job";
+console.log(john[xyz]);
+
+john.lastName = "Miller";
+john["job"] = "programer";
+
+console.log(john);
+
+var jane = new Object();
+jane.name = "Jane";
+jane.lastName = "Smith";
+jane["yearOfBirth"] = 1969;
+jane["job"] = "retired";
+jane["isMarried"] = true;
+
+console.log(jane);
+*/
+
+/////////////////////////////////////////////
+// Lecture: Objects and methods
+
+// v1.0
+/*
+var john = {
+  name: "John",
+  lastName: "Smith",
+  yearOfBirth: 1990,
+  job: "teacher",
+  isMarried: false,
+  family: ["Jane", "Mark", "Bob"],
+  calculateAge: function() {
+    return 2016 - this.yearOfBirth;
+  }
+};
+
+// console.log(john.calculateAge(1970));
+console.log(john.calculateAge());
+
+var age = john.calculateAge();
+john.age = age;
+
+console.log(john);
+*/
+
+//v2.0
+/*
+var john = {
+  name: "John",
+  lastName: "Smith",
+  yearOfBirth: 1990,
+  job: "teacher",
+  isMarried: false,
+  family: ["Jane", "Mark", "Bob"],
+  calculateAge: function() {
+    // return 2016 - this.yearOfBirth;
+    this.age = 2016 - this.yearOfBirth;
+  }
+};
+
+john.calculateAge();
+console.log(john);
+
+var mike = {
+  yearOfBirth: 1950,
+  calculateAge: function() {
+    this.age = 2016 - this.yearOfBirth;
+  }
+};
+
+mike.calculateAge();
+console.log(mike);
+*/
+
+/////////////////////////////////////////////
+// Lecture: Loops
+/*
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+var names = ["John", "Jane", "Mary", "Mark", "Bob"];
+
+// for loops
+// for (var i = 0; i < names.length; i++) {
+//   console.log(names[i]);
+// }
+
+// for (var i = names.length - 1; i >= 0; i--) {
+//   console.log(names[i]);
+// }
+
+// while loops
+var i = 0;
+while (i < names.length) {
+  console.log(names[i]);
+  i++;
+}
+
+for (var i = 0; i <= 5; i++) {
+  console.log(i);
+  if (i === 3) {
+    break;
+  }
+}
+
+for (var i = 0; i <= 5; i++) {
+  if (i === 3) {
+    continue;
+  }
+
+  console.log(i);
+}
+*/
+
+/////////////////////////////////////////////
+// Coding Challenge 2
